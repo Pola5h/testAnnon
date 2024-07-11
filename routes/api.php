918697be46c01441f80558af8eeca5f0154b6563
Pola5h <kzaman3055@gmail.com>
2024-07-11 20:04:19 +0600

    fix: remove middleware from api route

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+
     Route::apiResource('users', UserController::class);
     Route::apiResource('organizations', OrganizationController::class);
     Route::apiResource('contracts', ContractController::class);
-});
